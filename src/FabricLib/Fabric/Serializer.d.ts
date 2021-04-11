478dba9cec35e1b6322a7d21b7d78128f0cce103
suyoin <19378617+suyoin@users.noreply.github.com>
2021-04-11 18:06:24 -0500

    BuiltinSerializers had an error

--- src/FabricLib/Fabric/Serializer.d.ts
+++
@@ -1,7 +0,0 @@
-import Fabric from "../Fabric";
-import BuiltInSerializers from "";
-
-declare abstract class Serializer {
-	readonly _serializers: LuaMetatable<{ __index }>;
-	constructor(fabric: Fabric);
-}
