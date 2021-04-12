e47aed68bbcac2a272a2e92fb93efe2ed33826d9
suyoin <19378617+suyoin@users.noreply.github.com>
2021-04-11 20:36:05 -0500

    RoactUnitProps accounts for `_addLayerData` RoactUnitProps looks for ref validity get rid of namespace merge

--- src/FabricLib/index.d.ts
+++ src/FabricLib/index.d.ts
@@ -16,26 +16,28 @@ declare global {
 }
 
 declare namespace FabricLib {
-	export { Fabric, useTags, useReplication, useRoact, useBatching, useServiceUnits };
-}
-
-declare namespace FabricLib {
 	export type LiteralUnion<T extends string> = T | (Pick<string, never> & { _?: never });
 	export type NonNullableObject<T extends object> = {
 		[K in keyof T]: T[K] extends undefined ? never : T[K];
 	};
 	export type ThisFabricUnit<T extends keyof FabricUnits> = Unit<T> & FabricUnits[T];
 	export type PickByUnitRef<TRef> = {
-		[P in keyof FabricUnits]: TRef extends Required<FabricUnits[P]>["ref"]["name"]
-			? FabricUnits[P]
-			: never;
+		[P in keyof FabricUnits]: TRef extends Required<FabricUnits[P]>["ref"]["name"] ? FabricUnits[P] : never;
 	}[keyof FabricUnits]["name"];
 
-	export type RoactUnitProps<TFabricUnitNames extends keyof FabricUnits> = {
-		[key in TFabricUnitNames]: Partial<Unit<key>["data"]>;
-	};
+	export type PickByInstanceRef<TRef extends Instance> = {
+		[P in keyof FabricUnits]: TRef extends Required<FabricUnits[P]>["ref"] ? FabricUnits[P] : never;
+	}[keyof FabricUnits]["name"];
 
 	export { UnitDefinition };
+
+	export type RoactUnitProps<TInstance extends Instance, TFabricUnitNames extends PickByInstanceRef<TInstance>> = {
+		[key in TFabricUnitNames]: Required<FabricUnits[key]>["_addLayerData"] extends {}
+			? Required<FabricUnits[key]>["_addLayerData"]
+			: Partial<FabricUnits[key]["data"]>;
+	};
+
+	export { Fabric, useTags, useReplication, useRoact, useBatching, useServiceUnits };
 }
 
 export = FabricLib;
