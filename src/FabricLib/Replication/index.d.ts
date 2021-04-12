51a0e93bd17656d21b81de41a6df0ba9e47b4905
suyoin <19378617+suyoin@users.noreply.github.com>
2021-04-11 19:31:49 -0500

    Transmitter was not getting registered

--- src/FabricLib/Replication/index.d.ts
+++ src/FabricLib/Replication/index.d.ts
@@ -1,14 +1,31 @@
-import FabricLib from "..";
+import { ThisFabricUnit } from "..";
 import Fabric from "../Fabric";
 import { UnitDefinition } from "../Fabric/Types";
 
 interface ReplicatedDefinition extends UnitDefinition<"Replicated"> {
-	ref: FabricLib.ThisFabricUnit<keyof FabricUnits>;
+	ref?: ThisFabricUnit<keyof FabricUnits>;
+}
+
+interface TransmitterDefinition extends UnitDefinition<"Transmitter"> {
+	ref?: ThisFabricUnit<keyof FabricUnits>;
+
+	send(transmitEvent: string, transmitData?: unknown): void;
+
+	sendWithPredictiveLayer(layerData: object, transmitEvent: string, transmitData?: unknown): void;
+
+	broadcast(transmitEvent: string, transmitData: unknown): void;
+
+	sendTo(player: Player, transmitEvent: string, transmitData: unknown): void;
 }
 
 declare global {
 	interface FabricUnits {
 		Replicated: ReplicatedDefinition;
+
+		/** This unit should never be manually created.
+		 * @hidden
+		*/
+		Transmitter: TransmitterDefinition;
 	}
 }
 
