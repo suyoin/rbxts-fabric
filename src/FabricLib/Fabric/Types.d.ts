6658bcf4d0555dc915db473f153f417fab1d9053
suyoin <19378617+suyoin@users.noreply.github.com>
2021-05-28 15:47:01 -0500

    `defaults` can only be typed if `data` is an object

--- src/FabricLib/Fabric/Types.d.ts
+++ src/FabricLib/Fabric/Types.d.ts
@@ -42,7 +42,7 @@ interface UnitDefinition<T extends keyof FabricUnits> {
 	schema?: (value: unknown) => boolean;
 	refCheck?: string[] | ((ref: unknown) => boolean);
 
-	defaults?: Partial<FabricUnits[T]["data"]>;
+	defaults?: Required<FabricUnits[T]>["data"] extends {} ? Partial<FabricUnits[T]["data"]> : never;
 
 	units?: {
 		[key in keyof FabricUnits]?: Required<FabricUnits[key]>["_addLayerData"] extends {}
