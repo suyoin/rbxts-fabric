85d44c5d201fbf46811aa1938576b342a35fba32
suyoin <19378617+suyoin@users.noreply.github.com>
2021-05-28 14:08:08 -0500

    use InferDataType<T> instead of `defaults`

--- src/FabricLib/Fabric/Unit.d.ts
+++ src/FabricLib/Fabric/Unit.d.ts
@@ -1,11 +1,11 @@
-import { LiteralUnion, NonNullableObject, ThisFabricUnit } from "..";
+import { InferDataType, LiteralUnion, NonNullableObject, ThisFabricUnit } from "..";
 import Fabric from "../Fabric";
 
 declare abstract class Unit<T extends keyof FabricUnits> {
 	name: T;
 
-	data?: FabricUnits[T]["data"];
-	lastData?: FabricUnits[T]["data"];
+	data?: InferDataType<T>;
+	lastData?: Unit<T>["data"];
 
 	fabric: Fabric;
 
@@ -13,17 +13,12 @@ declare abstract class Unit<T extends keyof FabricUnits> {
 
 	fire(eventName: LiteralUnion<"destroy">, ...args: unknown[]): void;
 	on(eventName: "destroy", callback: () => void): () => void;
-	on(eventName: "loaded", callback: (newData: FabricUnits[T]["defaults"]) => void): () => void;
-	on(
-		eventName: "updated",
-		callback: (newData: FabricUnits[T]["defaults"], lastData: FabricUnits[T]["lastData"]) => void,
-	): () => void;
+	on(eventName: "loaded", callback: (newData: InferDataType<T>) => void): () => void;
+	on(eventName: "updated", callback: (newData: InferDataType<T>, lastData: InferDataType<T>) => void): () => void;
 	on(eventName: string, callback: (...args: unknown[]) => void): () => void;
 
-	get(): FabricUnits[T]["defaults"];
-	get<TKey extends keyof Required<FabricUnits[T]>["defaults"]>(
-		key: TKey,
-	): Required<FabricUnits[T]>["defaults"][TKey] | undefined;
+	get(): InferDataType<T>;
+	get<TKey extends keyof InferDataType<T>>(key: TKey): InferDataType<T>[TKey] | undefined;
 
 	getUnit<TAdd extends keyof FabricUnits>(unitResolvable: TAdd): ThisFabricUnit<TAdd> | undefined;
 	getOrCreateUnit<TAdd extends keyof FabricUnits>(unitResolvable: TAdd): ThisFabricUnit<TAdd>;
@@ -33,12 +28,12 @@ declare abstract class Unit<T extends keyof FabricUnits> {
 	addLayer<
 		TLayerData extends Required<FabricUnits[T]>["_addLayerData"] extends {}
 			? Required<FabricUnits[T]>["_addLayerData"]
-			: FabricUnits[T]["defaults"]
+			: InferDataType<T>
 	>(scope: unknown, data: NonNullableObject<TLayerData>): void;
 	mergeBaseLayer<
 		TLayerData extends Required<FabricUnits[T]>["_addLayerData"] extends {}
 			? Required<FabricUnits[T]>["_addLayerData"]
-			: FabricUnits[T]["defaults"]
+			: InferDataType<T>
 	>(data: NonNullableObject<TLayerData>): void;
 	removeLayer(scope: unknown): void;
 
