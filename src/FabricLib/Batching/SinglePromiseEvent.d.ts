90c3541926d3d7d76d018b9b45443bf2c897d5a5
suyoin <19378617+suyoin@users.noreply.github.com>
2021-05-28 10:34:53 -0500

    format SinglePromiseEvent

--- src/FabricLib/Batching/SinglePromiseEvent.d.ts
+++ src/FabricLib/Batching/SinglePromiseEvent.d.ts
@@ -1,5 +1,9 @@
 declare class SinglePromiseEvent {
-	constructor(executor: (callback: Callback) => (...[resolve, reject, onCancel]: Parameters<Parameters<PromiseConstructor["defer"]>[0]>) => unknown);
+	constructor(
+		executor: (
+			callback: Callback,
+		) => (...[resolve, reject, onCancel]: Parameters<Parameters<PromiseConstructor["defer"]>[0]>) => unknown,
+	);
 
 	Connect(callback: Callback): { Disconnect(): void };
 }
